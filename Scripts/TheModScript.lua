c22b09f3447c747a0a98bf6ea8ca65921cec65e6
Aaron <rekked.noraa@gmail.com>
2015-10-23 00:26:16 +1300

    Initial commit. Needs testing.

---
+++ Scripts/TheModScript.lua
@@ -0,0 +1,36 @@
+--TheModScript
+-------------------------------------------------------------------------------
+if TheModScript == nil then
+	TheModScript = EternusEngine.ModScriptClass.Subclass("TheModScript")
+end
+
+-------------------------------------------------------------------------------
+function TheModScript:Constructor()
+end
+
+ -------------------------------------------------------------------------------
+ -- Called once from C++ at engine initialization time
+function TheModScript:Initialize()
+	Eternus.CraftingSystem:ParseRecipeFile("Data/Crafting/Decor_Recipes.txt")
+	Eternus.CraftingSystem:ParseRecipeFile("Data/Crafting/Tools_Recipes.txt")
+	Eternus.CraftingSystem:ParseRecipeFile("Data/Crafting/Weapon_Recipes.txt")
+	Eternus.CraftingSystem:ParseRecipeFile("Data/Crafting/Repair_Recipes.txt")
+end
+
+-------------------------------------------------------------------------------
+-- Called from C++ when the current game enters 
+function TheModScript:Enter()
+end
+
+-------------------------------------------------------------------------------
+-- Called from C++ when the game leaves it current mode
+function TheModScript:Leave()
+end
+
+-------------------------------------------------------------------------------
+-- Called from C++ every update tick
+function TheModScript:Process(dt)
+end
+
+-------------------------------------------------------------------------------
+EntityFramework:RegisterModScript(TheModScript)
